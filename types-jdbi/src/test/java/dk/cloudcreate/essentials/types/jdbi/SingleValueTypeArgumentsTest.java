2b988ccb8717ac560de72b90e4bf71be98465f4d
Jeppe Cramon <jeppe@cloudcreate.dk>
2022-01-02 23:01:25 +0100

    Added AmountArgumentFactory/CountryCodeArgumentFactory/CurrencyCodeArgumentFactory/EmailAddressArgumentFactory/PercentageArgumentFactory Added tests for new ArgumentFactories and types

--- types-jdbi/src/test/java/dk/cloudcreate/essentials/types/jdbi/SingleValueTypeArgumentsTest.java
+++ types-jdbi/src/test/java/dk/cloudcreate/essentials/types/jdbi/SingleValueTypeArgumentsTest.java
@@ -16,9 +16,12 @@
 
 package dk.cloudcreate.essentials.types.jdbi;
 
+import dk.cloudcreate.essentials.types.*;
 import dk.cloudcreate.essentials.types.jdbi.types.*;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,6 +29,12 @@ class SingleValueTypeArgumentsTest {
     @Test
     void test_jdbi_argument_factories() {
         Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.registerArgument(new AmountArgumentFactory());
+        jdbi.registerArgument(new CountryCodeArgumentFactory());
+        jdbi.registerArgument(new CurrencyCodeArgumentFactory());
+        jdbi.registerArgument(new EmailAddressArgumentFactory());
+        jdbi.registerArgument(new PercentageArgumentFactory());
+
         jdbi.registerArgument(new OrderIdArgumentFactory());
         jdbi.registerArgument(new CustomerIdArgumentFactory());
         jdbi.registerArgument(new ProductIdArgumentFactory());
@@ -36,18 +45,35 @@ class SingleValueTypeArgumentsTest {
                                    "id BIGINT PRIMARY KEY, " +
                                    "customer_id VARCHAR, " +
                                    "product_id VARCHAR, " +
-                                   "account_id BIGINT)");
+                                   "account_id BIGINT," +
+                                   "amount NUMERIC(20, 3)," +
+                                   "country VARCHAR," +
+                                   "currency VARCHAR," +
+                                   "email VARCHAR," +
+                                   "percentage NUMERIC(20, 2)" +
+                                   ")");
 
             var orderId    = OrderId.random();
             var customerId = CustomerId.random();
             var productId  = ProductId.random();
             var accountId  = AccountId.random();
+            var amount     = Amount.of("123.456");
+            var country    = CountryCode.of("DK");
+            var currency   = CurrencyCode.of("DKK");
+            var email      = EmailAddress.of("john@nonexistingdomain.com");
+            var percentage = Percentage.from("40.5%");
 
-            handle.inTransaction(_handle -> handle.createUpdate("INSERT INTO orders(id, customer_id, product_id, account_id) VALUES (:id, :customerId, :productId, :accountId)")
+            handle.inTransaction(_handle -> handle.createUpdate("INSERT INTO orders(id, customer_id, product_id, account_id, amount, country, currency, email, percentage) " +
+                                                                        "VALUES (:id, :customerId, :productId, :accountId, :amount, :country, :currency, :email, :percentage)")
                                                   .bind("id", orderId)
                                                   .bind("customerId", customerId)
                                                   .bind("productId", productId)
                                                   .bind("accountId", accountId)
+                                                  .bind("amount", amount)
+                                                  .bind("country", country)
+                                                  .bind("currency", currency)
+                                                  .bind("email", email)
+                                                  .bind("percentage", percentage)
                                                   .execute());
             var results = handle.inTransaction(_handle -> handle.createQuery("SELECT * from orders WHERE id = :id")
                                                                 .bind("id", orderId)
@@ -60,6 +86,11 @@ class SingleValueTypeArgumentsTest {
             assertThat(result.get("customer_id")).isEqualTo(customerId.value());
             assertThat(result.get("product_id")).isEqualTo(productId.value());
             assertThat((long) result.get("account_id")).isEqualTo(accountId.value());
+            assertThat(result.get("amount")).isEqualTo(amount.value());
+            assertThat(result.get("country")).isEqualTo(country.value());
+            assertThat(result.get("currency")).isEqualTo(currency.value());
+            assertThat(result.get("email")).isEqualTo(email.value());
+            assertThat(result.get("percentage")).isEqualTo(percentage.value());
         });
 
     }
